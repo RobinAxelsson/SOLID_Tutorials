b602d5d2d17690034e776f919673a25830b188a5
RobinAxelsson <axelsson.robin@outlook.com>
2021-04-15 09:43:26 +0200

    finished solid, work more on SRP

---
+++ ISP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ISP
+{
+    /*
+     *  A client should not depend on an Interface it doesn't use!
+     *  Break the application apart to lots of small interfaces!
+     *  Library example, DVD doesn't have pages, audio books doesn't either.
+     * 
+     * 
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
